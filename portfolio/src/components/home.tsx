96e421a95ed308c2b869ae472548190cc4a5bb28
Admin <kentaki771@gmail.com>
2024-08-16 00:27:13 +0800

    test

--- portfolio/src/components/home.tsx
+++ portfolio/src/components/home.tsx
@@ -20,7 +20,6 @@ import Card from "./UI Componemts/ProjectCard";
 import expData from "./data/ExpData";
 import Project from "./data/ProjectData";
 import Skills from "./data/SkillsData";
-import ExpCard from "./UI Componemts/ExperienceCard";
 import { useTheme } from "next-themes";
 import Link from "next/link";
 import AOS from "aos";
@@ -75,22 +74,22 @@ const Home = () => {
             <Image
               data-aos="fade-down"
               // src={`/photo-${theme === "light" ? "light" : "dark"}.jpg`}
-              src={`/shiveshnandan.jpg`}
+              src={`/mike.jpg`}
               height={10000}
               width={10000}
               alt={"Profile Photo"}
-              className="border rounded-full w-[90px] mb-8 border-[#333] dark:border-white"
+              className="border rounded-full w-[130px] mb-8 border-[#333] dark:border-white"
             ></Image>
             <div data-aos="fade-right" className="">
-              <h1 className="text-3xl font-[700] ">Shivesh Nandan</h1>
+              <h1 className="text-3xl font-[700] ">Hakim Saricala</h1>
               <p className="mt-1">
                 <TypeAnimation
                   sequence={[
-                    "Software engineering student",
+                    "BSIT student",
                     2000,
-                    "MERN stack developer",
+                    "Full stack developer",
                     2000,
-                    "Freelancer",
+                    "Mobile Developer",
                     2000,
                   ]}
                   wrapper="span"
@@ -104,14 +103,14 @@ const Home = () => {
               data-aos="fade-left"
               className=" pt-6 text-sm text-[#333] dark:text-[#d8d8d8] leading-[21px] "
             >
-              Trying to build a faster web application. Learning about web
-              development, React / Next.js and data structure and algorithm.
+              Trying to build a faster web and mobile application. Learning
+              about web development and Mobile development.
             </h1>
             <div
               data-aos="fade-up"
               className="flex mt-3 justify-center flex-wrap"
             >
-              <Link href={"https://github.com/shiveshnandan"} target="_blank">
+              <Link href={"https://github.com/HakimSaricala"} target="_blank">
                 <Button
                   variant={"outline"}
                   size={"sm"}
@@ -119,7 +118,7 @@ const Home = () => {
                 >
                   <GithubIcon className="w-6 px-1" />
                   <p className="font-[500] text-sm tracking-wide">
-                    @ShiveshNandan
+                    @HakimSaricala
                   </p>
                   <p className=" text-[11px] px-2 text-gray-500 flex pt-1">
                     Github
@@ -128,7 +127,7 @@ const Home = () => {
                 </Button>
               </Link>
               <Link
-                href={"https://linkedin.com/in/shiveshnandan"}
+                href={"https://www.linkedin.com/in/hakim-saricala-670724171/"}
                 target="_blank"
               >
                 <Button
@@ -138,31 +137,10 @@ const Home = () => {
                 >
                   <LinkedinIcon className="w-6 px-1" />
                   <p className="font-[500] text-sm tracking-wide">
-                    @shiveshnandan
+                    @HakimSaricala
                   </p>
                   <p className=" text-[11px] px-2 text-gray-500 flex pt-1">
                     Linkedin
-                  </p>
-                  <ArrowUpRightFromSquare className="h-3 w-3 text-gray-500" />
-                </Button>
-              </Link>
-              <Link
-                href={
-                  "https://drive.google.com/file/d/1pLK09sfY0jLksIqwSOiMBNH2QXBIW6xC/view?usp=sharing"
-                }
-                target="_blank"
-              >
-                <Button
-                  variant={"outline"}
-                  size={"sm"}
-                  className="dark:bg-[#333] py-4 mx-2 my-1"
-                >
-                  <NotebookTextIcon className="w-6 px-1" />
-                  <p className="font-[500] text-sm tracking-wide">
-                    Shivesh Nandan
-                  </p>
-                  <p className=" text-[11px] px-2 text-gray-500 flex pt-1">
-                    Resume
                   </p>
                   <ArrowUpRightFromSquare className="h-3 w-3 text-gray-500" />
                 </Button>
@@ -187,30 +165,18 @@ const Home = () => {
               data-aos-anchor-placement="top-bottom"
               className="line-clamp- my-3 tracking-[0.3px] "
             >
-              Hello! I&apos;m final-year CSE student at USICT, GGSIPU Delhi.
-              Over the past year, I&apos;ve immersed myself in web development,
-              creating projects with frameworks like React.js and now focusing
-              on Next.js. This portfolio showcases my work using Next.js,
-              Tailwind CSS, and ShadCN to build dynamic, responsive
-              applications. I&apos;m eager to find internships or freelance
-              opportunities to expand my skills further. Besides coding, I enjoy
-              photography. Let&apos;s connect and create something amazing
-              together!
+              Hello! I&apos;m a fourth-year BSIT student at MSEUF, Lucena. Over
+              the past few years, I&apos;m trying to learn and develop my skills
+              using different frameworks. This portfolio showcases all my school
+              works throughout the years . I&apos;m eager to find internships
+              opportunities to expand my skills further . Let&apos;s connect and
+              create something amazing together!
             </p>
             <div
               data-aos="fade-left"
               data-aos-anchor-placement="top-bottom"
               className="flex antialiased justify-end text-sky-600 dark:text-sky-500 relative font-bold"
-            >
-              <Button
-                onClick={openModal}
-                variant={"outline"}
-                className="text-sky-600 dark:text-sky-500 font-bold w-fit"
-              >
-                {" "}
-                Read More <ArrowUpRightFromSquare className="mx-2 h-4 w-4" />
-              </Button>
-            </div>
+            ></div>
 
             {isModalOpen && (
               <Modal Heading="About Me" onClose={closeModal}>
@@ -236,14 +202,14 @@ const Home = () => {
                   data-aos-anchor-placement="top-bottom"
                   variant="outline"
                   key={index}
-                  className="my-1 mx-1 text-[14px]"
+                  className="my-1 mx-1 text-[14px] "
                 >
                   <Image
                     src={`/Skills/${skill}.png`}
                     alt={`${skill}`}
                     height={18}
                     width={18}
-                    className="mr-2"
+                    className="mr-2 "
                   />
                   {skill}
                 </Badge>
@@ -290,68 +256,6 @@ const Home = () => {
                 </div>
               )}
             </div>
-            <div
-              data-aos="fade-left"
-              data-aos-anchor-placement="top-bottom"
-              className="flex justify-end"
-            >
-              <Button
-                onClick={openModal2}
-                variant={"outline"}
-                className="text-sky-600 dark:text-sky-500 font-bold w-fit"
-              >
-                {" "}
-                View All Projects{" "}
-                <ArrowUpRightFromSquare className="mx-2 h-4 w-4" />
-              </Button>
-            </div>
-
-            {isModalOpen2 && (
-              <Modal Heading="Projects" onClose={closeModal2}>
-                <div className="flex flex-wrap">
-                  {Project.map((project: any, index: any) => (
-                    <Card
-                      Title={project.heading}
-                      Description={project.summary}
-                      link={project.link}
-                      code={project.code}
-                      Img={project.img}
-                      techUse={project.tech}
-                      key={index}
-                    />
-                  ))}
-                </div>
-              </Modal>
-            )}
-          </div>
-
-          {/* ============================== Experience ============================ */}
-
-          <div id="experience" className="flex flex-col pt-[14vh]">
-            <h1
-              data-aos="fade-right"
-              data-aos-anchor-placement="top-bottom"
-              className="text-2xl underline decoration-sky-500 dark:decoration-sky-500/80 underline-offset-[5px] decoration-4 tracking-wide antialiased "
-            >
-              Experience
-            </h1>
-            <div className="my-2 rounded-xl bg">
-              <div className="border-l-8 ">
-                {!Start
-                  ? "loading ! "
-                  : expData.map((exp, index) => (
-                      <div key={index}>
-                        <ExpCard
-                          img={`/Experience${exp.img}`}
-                          companyName={exp.companyName}
-                          role={exp.role}
-                          description={exp.description}
-                          timePeriod={exp.timePeriod}
-                        ></ExpCard>
-                      </div>
-                    ))}
-              </div>
-            </div>
           </div>
 
           {/* ============================== Contact me ============================ */}
@@ -364,19 +268,24 @@ const Home = () => {
             >
               Get in Touch
             </h1>
-            <p data-aos="fade-right"
-              data-aos-anchor-placement="top-bottom" className="mt-3 text-sm">Feel free to reach out to me for any queries or collaborations.</p>
-            <Getintouch/>
+            <p
+              data-aos="fade-right"
+              data-aos-anchor-placement="top-bottom"
+              className="mt-3 text-sm"
+            >
+              Feel free to reach out to me for any queries or collaborations.
+            </p>
+            <Getintouch />
             <div className="flex flex-col my-2 rounded-xl items-center">
               <h1
                 data-aos="fade-up"
                 data-aos-anchor-placement="top-bottom"
                 className="m-auto my-4 text-center"
               >
-              OR 
+                OR
               </h1>
               <div className="flex">
-                <Link href={"https://instagram.com/shivesh_nandan"}>
+                <Link href={""}>
                   <Button
                     data-aos="fade-up"
                     data-aos-anchor-placement="top-bottom"
@@ -387,7 +296,7 @@ const Home = () => {
                     <InstagramIcon />
                   </Button>
                 </Link>
-                <Link href={"https://github.com/ShiveshNandan"}>
+                <Link href={""}>
                   <Button
                     data-aos="fade-up"
                     data-aos-anchor-placement="top-bottom"
@@ -399,11 +308,7 @@ const Home = () => {
                     <TwitterIcon />{" "}
                   </Button>
                 </Link>
-                <Link
-                  href={
-                    "mailto:kshivesh929@gmail.com?subject=Hello%20there&body=How%20are%20you%3F"
-                  }
-                >
+                <Link href={""}>
                   <Button
                     data-aos="fade-up"
                     data-aos-anchor-placement="top-bottom"
@@ -438,10 +343,6 @@ const Home = () => {
                 <span className="text-sky-600 dark:text-sky-500">Vercel</span>
               </Link>
               .
-            </h1>
-            <h1 className="mb-6 py-1">
-              © 2024 <b className="font-[700] tracking-[1px]">Shivesh Nandan</b>
-              . All rights reserved.
             </h1>
           </div>
         </>
--- portfolio/src/components/navbar.tsx
+++ portfolio/src/components/navbar.tsx
@@ -2,11 +2,7 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import Change from "./themeChange";
-import {
-  Sheet,
-  SheetContent,
-  SheetTrigger,
-} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Command } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { Link } from "react-scroll";
@@ -26,30 +22,24 @@ const Navbar = () => {
   const [isModalOpen, setModalOpen] = useState(false);
   const openModal = () => setModalOpen(true);
   const closeModal = () => setModalOpen(false);
-  const [selected, setselected] = useState("home")
-
+  const [selected, setselected] = useState("home");
 
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     if (isModalOpen) {
-      document.body.classList.add('modal-open');
+      document.body.classList.add("modal-open");
     } else {
-      document.body.classList.remove('modal-open');
+      document.body.classList.remove("modal-open");
     }
 
     return () => {
-      document.body.classList.remove('modal-open');
+      document.body.classList.remove("modal-open");
     };
   }, [isModalOpen]);
 
-
-
-
-
-  const hoverEff = "mx-2 px-1 font-[600] hover:text-sky-600 dark:hover:text-sky-500";
-
-
+  const hoverEff =
+    "mx-2 px-1 font-[600] hover:text-sky-600 dark:hover:text-sky-500";
 
   return (
     <>
@@ -58,26 +48,32 @@ const Navbar = () => {
       ) : (
         <div className={`w-full fixed top-0 z-[10] `}>
           <div className="flex w-[10vw]">
-          {isModalOpen && (
-            <Modal Heading="Command" onClose={closeModal}>
-            <Comand/>
-          </Modal>
-        )}
-        </div>
+            {isModalOpen && (
+              <Modal Heading="Command" onClose={closeModal}>
+                <Comand />
+              </Modal>
+            )}
+          </div>
           <div
             data-aos="fade-down"
             data-aos-delay="50"
             className="flex backdrop-blur7 w-full m-auto lg:w-[750px] rounded-b-xl justify-between px-6 "
           >
             <div className={`menu md:flex items-center hidden mt-5`}>
-              
               <Link
                 to="home"
                 smooth={true}
                 duration={500}
                 className="cursor-pointer"
               >
-                <h1 onClick={() => {setselected("home")}} className={`${selected === "home" ? "text-sky-600 dark:text-sky-500" : ""} ${hoverEff}`}>
+                <h1
+                  onClick={() => {
+                    setselected("home");
+                  }}
+                  className={`${
+                    selected === "home" ? "text-sky-600 dark:text-sky-500" : ""
+                  } ${hoverEff}`}
+                >
                   Home
                 </h1>
               </Link>
@@ -87,7 +83,14 @@ const Navbar = () => {
                 duration={500}
                 className="cursor-pointer"
               >
-                <h1 onClick={() => {setselected("about")}} className={`${selected === "about" ? "text-sky-600 dark:text-sky-500" : ""} ${hoverEff}`}>
+                <h1
+                  onClick={() => {
+                    setselected("about");
+                  }}
+                  className={`${
+                    selected === "about" ? "text-sky-600 dark:text-sky-500" : ""
+                  } ${hoverEff}`}
+                >
                   About
                 </h1>
               </Link>
@@ -97,7 +100,16 @@ const Navbar = () => {
                 duration={500}
                 className="cursor-pointer"
               >
-                <h1 onClick={() => {setselected("skills")}} className={`${selected === "skills" ? "text-sky-600 dark:text-sky-500" : ""} ${hoverEff}`}>
+                <h1
+                  onClick={() => {
+                    setselected("skills");
+                  }}
+                  className={`${
+                    selected === "skills"
+                      ? "text-sky-600 dark:text-sky-500"
+                      : ""
+                  } ${hoverEff}`}
+                >
                   Skills
                 </h1>
               </Link>
@@ -107,27 +119,36 @@ const Navbar = () => {
                 duration={500}
                 className="cursor-pointer"
               >
-                <h1 onClick={() => {setselected("projects")}} className={`${selected === "projects" ? "text-sky-600 dark:text-sky-500" : ""} ${hoverEff}`}>
+                <h1
+                  onClick={() => {
+                    setselected("projects");
+                  }}
+                  className={`${
+                    selected === "projects"
+                      ? "text-sky-600 dark:text-sky-500"
+                      : ""
+                  } ${hoverEff}`}
+                >
                   Projects
                 </h1>
               </Link>
-              <Link
-                to="experience"
-                smooth={true}
-                duration={500}
-                className="cursor-pointer"
-              >
-                <h1 onClick={() => {setselected("experience")}} className={`${selected === "experience" ? "text-sky-600 dark:text-sky-500" : ""} ${hoverEff}`}>
-                  Experience
-                </h1>
-              </Link>
+
               <Link
                 to="connect"
                 smooth={true}
                 duration={500}
                 className="cursor-pointer"
               >
-                <h1 onClick={() => {setselected("contact")}} className={`${selected === "contact" ? "text-sky-600 dark:text-sky-500" : ""} ${hoverEff}`}>
+                <h1
+                  onClick={() => {
+                    setselected("contact");
+                  }}
+                  className={`${
+                    selected === "contact"
+                      ? "text-sky-600 dark:text-sky-500"
+                      : ""
+                  } ${hoverEff}`}
+                >
                   Connect
                 </h1>
               </Link>
@@ -156,11 +177,11 @@ const Navbar = () => {
                   </Button>
                 </SheetTrigger>
 
-                <SheetContent onClick={() => setIsOpen(false)} className="pt-12 sm:w-[40vw] ">
-                  <div
-                    
-                    className="flex flex-col my-3 items-center "
-                  >
+                <SheetContent
+                  onClick={() => setIsOpen(false)}
+                  className="pt-12 sm:w-[40vw] "
+                >
+                  <div className="flex flex-col my-3 items-center ">
                     <Link
                       to="home"
                       smooth={true}
@@ -169,11 +190,7 @@ const Navbar = () => {
                       onClick={() => setIsOpen(false)}
                     >
                       <div className="flex w-full justify-center">
-                        <h1
-                          className="flex mt-8 mb-2 "
-                        >
-                          Home
-                        </h1>
+                        <h1 className="flex mt-8 mb-2 ">Home</h1>
                       </div>
                     </Link>
                     <Link
@@ -184,11 +201,7 @@ const Navbar = () => {
                       onClick={() => setIsOpen(false)}
                     >
                       <div className="flex w-full justify-center">
-                        <h1
-                          className="flex mt-8 mb-2"
-                        >
-                          About
-                        </h1>
+                        <h1 className="flex mt-8 mb-2">About</h1>
                       </div>
                     </Link>
                     <Link
@@ -196,14 +209,10 @@ const Navbar = () => {
                       smooth={true}
                       duration={500}
                       className="cursor-pointer flex w-full border-b border-[#efefef] rounded-sm"
-                        onClick={() => setIsOpen(false)}
+                      onClick={() => setIsOpen(false)}
                     >
                       <div className="flex w-full justify-center">
-                        <h1
-                          className="flex mt-8 mb-2"
-                        >
-                          Skills
-                        </h1>
+                        <h1 className="flex mt-8 mb-2">Skills</h1>
                       </div>
                     </Link>
                     <Link
@@ -211,44 +220,22 @@ const Navbar = () => {
                       smooth={true}
                       duration={500}
                       className="cursor-pointer flex w-full border-b border-[#efefef] rounded-sm"
-                        onClick={() => setIsOpen(false)}
+                      onClick={() => setIsOpen(false)}
                     >
                       <div className="flex w-full justify-center">
-                        <h1
-                          className="flex mt-8 mb-2"
-                        >
-                          Projects
-                        </h1>
+                        <h1 className="flex mt-8 mb-2">Projects</h1>
                       </div>
                     </Link>
-                    <Link
-                      to="experience"
-                      smooth={true}
-                      duration={500}
-                      className="cursor-pointer flex w-full border-b border-[#efefef] rounded-sm"
-                        onClick={() => setIsOpen(false)}
-                    >
-                      <div className="flex w-full justify-center">
-                        <h1
-                          className="flex mt-8 mb-2"
-                        >
-                          Experience
-                        </h1>
-                      </div>
-                    </Link>
+
                     <Link
                       to="connect"
                       smooth={true}
                       duration={500}
                       className="cursor-pointer flex w-full border-b border-[#efefef] rounded-sm"
-                        onClick={() => setIsOpen(false)}
+                      onClick={() => setIsOpen(false)}
                     >
                       <div className="flex w-full justify-center">
-                        <h1
-                          className="flex mt-8 mb-2"
-                        >
-                          Connect
-                        </h1>
+                        <h1 className="flex mt-8 mb-2">Connect</h1>
                       </div>
                     </Link>
                   </div>
@@ -259,26 +246,11 @@ const Navbar = () => {
             </div>
             <div className="flex my-auto mt-8 mb-6 ">
               <div
-                data-aos="fade-down"
-                data-aos-delay="150"
-                className="flex justify-center flex-col w-10 h-10 overflow-hidden rounded-xl bg-[#d8d8d8] dark:bg-[#333] mx-2 cursor-pointer"
-              >
-
-                <div
-                className="h-full flex items-center "
-            onClick={openModal}
-          >
-              <Command className="w-full" />
-          </div>
-
-               
-              </div>
-              <div
                 // data-aos="fade-down"
                 // data-aos-delay="250"
                 className="flex justify-center flex-col w-10 h-10 overflow-hidden rounded-xl bg-[#d8d8d8] dark:bg-[#333] mx-2 cursor-pointer"
               >
-                <Change/>
+                <Change />
               </div>
             </div>
           </div>
